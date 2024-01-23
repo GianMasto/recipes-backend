ce42eb4c9ed6f860a23d752297889c9a3ddbc995
Gian Masto <gianmasto1@gmail.com>
2024-01-23 00:20:30 -0300

    Update recipes.ts

--- routes/recipes.ts
+++ routes/recipes.ts
@@ -1,13 +1,18 @@
 import express, { Request, Response } from "express";
 
+import { Recipe } from "../models/Recipe";
+import { IRecipe } from "../interfaces/IRecipe";
+
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-  res.send("GET recipes");
+router.get("/", async (req: Request, res: Response) => {
+  const recipes = await Recipe.find();
+  res.send(recipes);
 });
 
-router.post("/", (req: Request, res: Response) => {
-  res.send("POST recipes");
+router.post("/", async (req: Request<IRecipe>, res: Response) => {
+  const newRecipe = new Recipe(req.body);
+  res.send(newRecipe);
 });
 
 router.put("/", (req: Request, res: Response) => {
